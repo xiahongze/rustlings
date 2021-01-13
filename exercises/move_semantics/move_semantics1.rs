4f3ddbb88b591d0739d9d4ef36336393d2275ec3
Hong Xia <hongze.x@gmail.com>
2021-01-13 23:03:27 +1100

    move 1&2

--- exercises/move_semantics/move_semantics1.rs
+++ exercises/move_semantics/move_semantics1.rs
@@ -1,12 +1,10 @@
 // move_semantics1.rs
 // Make me compile! Execute `rustlings hint move_semantics1` for hints :)
 
-// I AM NOT DONE
-
 fn main() {
     let vec0 = Vec::new();
 
-    let vec1 = fill_vec(vec0);
+    let mut vec1 = fill_vec(vec0); // vec0 moved
 
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 
@@ -16,11 +14,11 @@ fn main() {
 }
 
 fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let mut vec = vec;
+    let mut vec = vec; // moved as mutable
 
     vec.push(22);
     vec.push(44);
     vec.push(66);
 
-    vec
+    vec // moved out
 }
--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -2,27 +2,30 @@
 // Make me compile without changing line 13!
 // Execute `rustlings hint move_semantics2` for hints :)
 
-// I AM NOT DONE
-
+/**
+ * METHODS
+ * 1. vec0.clone()
+ * 2. move vec0 with &vec0 and modify fill_vec with move and then to_owned or clone
+ * 3. as described below
+ *
+ **/
 fn main() {
-    let vec0 = Vec::new();
+    // mutable vec allows pushing elements
+    let mut vec0 = Vec::new();
 
-    let mut vec1 = fill_vec(vec0);
+    // borrowed a mutable ref to the mutable vec
+    fill_vec(&mut vec0);
 
     // Do not change the following line!
     println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
 
-    vec1.push(88);
+    vec0.push(88);
 
-    println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
+    println!("{} has length {} content `{:?}`", "vec1", vec0.len(), vec0);
 }
-
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let mut vec = vec;
-
+// without returning a value
+fn fill_vec(vec: &mut Vec<i32>) {
     vec.push(22);
     vec.push(44);
     vec.push(66);
-
-    vec
 }

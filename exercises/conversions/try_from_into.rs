54d0f8be8d455a329e90fcccf83c23a1feb0d168
Hong Xia <hongze.x@gmail.com>
2021-01-16 21:47:10 +1100

    try from without macro

--- exercises/conversions/try_from_into.rs
+++ exercises/conversions/try_from_into.rs
@@ -25,19 +25,57 @@ struct Color {
 // Tuple implementation
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = String;
-    fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {}
+    fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
+        let range = 0..256;
+        if range.contains(&tuple.0) && range.contains(&tuple.1) && range.contains(&tuple.2) {
+            Ok(Color {
+                red: tuple.0 as u8,
+                green: tuple.1 as u8,
+                blue: tuple.2 as u8,
+            })
+        } else {
+            Err("not in range".to_string())
+        }
+    }
 }
 
 // Array implementation
 impl TryFrom<[i16; 3]> for Color {
     type Error = String;
-    fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {}
+    fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+        let range = 0..256;
+        if range.contains(&arr[0]) && range.contains(&arr[1]) && range.contains(&arr[2]) {
+            Ok(Color {
+                red: arr[0] as u8,
+                green: arr[1] as u8,
+                blue: arr[2] as u8,
+            })
+        } else {
+            Err("not in range".to_string())
+        }
+    }
 }
 
 // Slice implementation
 impl TryFrom<&[i16]> for Color {
     type Error = String;
-    fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {}
+    fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
+        let range = 0..256;
+        if slice.len() != 3 {
+            Err("input slice is not of length 3".to_string())
+        } else if range.contains(&slice[0])
+            && range.contains(&slice[1])
+            && range.contains(&slice[2])
+        {
+            Ok(Color {
+                red: slice[0] as u8,
+                green: slice[1] as u8,
+                blue: slice[2] as u8,
+            })
+        } else {
+            Err("not in range".to_string())
+        }
+    }
 }
 
 fn main() {
